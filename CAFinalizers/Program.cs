563fba368d058f94be725b47d4aae16031cf0d5e
Mohamed Elkerba <mohamedelkerba0@gmial.com>
2024-09-25 15:37:50 +0300

    CAFINALIZERS

---
+++ CAFinalizers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CAFinalizers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MakeSomeGarbage();
+            Console.WriteLine($"Memory Used Befor Collecting : {GC.GetTotalMemory(false) :N0}");
+            GC.Collect(); //Explict Cleaning
+            Console.WriteLine($"Memory Used After Collection : {GC.GetTotalMemory(true):N0}");
+            Console.ReadKey();
+        }
+
+        static void MakeSomeGarbage() {
+            Version v;
+            for (int i = 0; i < 1000; i++) { 
+             v = new Version();
+            }
+        
+        }
+    }
+
+}
